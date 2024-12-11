7d2ed87c140c4ab3e2cdb0b365aaeaf5a4ae6020
mxxav <mx.xavier.xavier@outlook.pt>
2024-12-11 23:22:16 +0000

    resolver warnings

--- src/main/java/com/mx/gritsolutions/GritSolutionsApplication.java
+++ src/main/java/com/mx/gritsolutions/GritSolutionsApplication.java
@@ -1,9 +1,13 @@
 package com.mx.gritsolutions;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class GritSolutionsApplication {
 
 	public static void main(String[] args) {
